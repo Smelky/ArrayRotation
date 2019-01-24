018803786426354a35f36ebc261e8cc148dfed8c
Smelky <46086711+Smelky@users.noreply.github.com>
2019-01-24 12:23:22 +0200

    Changed

---
+++ src/com/company/ArrayRotation.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+        public class ArrayRotation {
+            private static Scanner scan = new Scanner(System.in);
+
+            public static void shift (int shiftElements, int arr[]) {
+                for (int q = 0; q < shiftElements; q++) {
+                    int k = arr[0];
+                    for (int i = 0; i < arr.length - 1; i++) {
+                        arr[i] = arr[i + 1];
+                    }
+                    arr[arr.length - 1] = k;
+                }
+                System.out.print(Arrays.toString(arr));
+            }
+
+            public static void main (String arg[]) {
+                System.out.print("Print array length: ");
+                int arrayLength;
+                arrayLength = scan.nextInt();
+                int [] arr = new int [arrayLength];
+                for (int f = 0; f < arrayLength; f++){
+                    System.out.print("Print the " + f + " array element: ");
+                    int arrayElemets;
+                    arrayElemets = scan.nextInt();
+                    arr[f] = arrayElemets;
+                }
+                System.out.print("Print, how many of elements should be moved to the array end: ");
+                int shiftElements;
+                shiftElements = scan.nextInt();
+                shift(shiftElements, arr);
+            }
+        }
+
